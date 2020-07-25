40c0d6ffcaca6214dcd5a35445bdc456b4c9d20f
Phoebe Lin <phoebe1318@yahoo.com>
2020-07-25 14:42:14 -0500

    level4 skeleton

--- src/levelFour.js
+++ src/levelFour.js
@@ -23,32 +23,32 @@ export default class levelFour extends Phaser.Scene {
         
         this.tubeGroup = this.physics.add.group();
         this.tube1 = this.tubeGroup.create(250,337, "tubeA").setScale(.5).setInteractive();
-          this.target1=this.tube1.angle=0;
+          this.target1=this.tube1.angle=90;
         this.tube2 = this.tubeGroup.create(249,411, "tubeC").setScale(.5).setInteractive();
-          this.target2=this.tube2.angle=180;
+          this.target2=this.tube2.angle=270;
           this.tube2.setOrigin(0.75,0.25);
         this.tube3 = this.tubeGroup.create(333,410, "tubeA").setScale(.5).setInteractive();
           this.tube3.angle=90; 
         //
         this.tube4 = this.tubeGroup.create(403,412, "tubeB").setScale(.5).setInteractive();
-          this.target4=this.tube4.angle=0; //0
+          this.target4=this.tube4.angle=270; 
           this.tube4.setOrigin(0.60,0.5);
         this.tube5 = this.tubeGroup.create(409,347, "tubeB").setScale(.5).setInteractive();
-          this.target5=this.tube5.angle=270;
+          this.target5=this.tube5.angle=0;
           this.tube5.setOrigin(0.70,0.5);
           
         this.tube6 = this.tubeGroup.create(342,351, "tubeC").setScale(.5).setInteractive();
           this.target6=this.tube6.angle=180;
           this.tube6.setOrigin(0.75,0.25);
         this.tube7 = this.tubeGroup.create(490,350, "tubeD").setScale(.5).setInteractive();
-          this.target7=this.tube7.angle=90; //90
+          this.target7=this.tube7.angle=0; 
           this.tube7.setOrigin(0.5,0.25);
           
         this.tube8 = this.tubeGroup.create(405,485, "tubeC").setScale(.5).setInteractive();
-          this.target8=this.tube8.angle=180;
+          this.target8=this.tube8.angle=90;
           this.tube8.setOrigin(0.75,0.25);
         this.tube9 = this.tubeGroup.create(490,485, "tubeD").setScale(.5).setInteractive();
-          this.target9=this.tube9.angle=90;
+          this.target9=this.tube9.angle=270;
           this.tube9.setOrigin(0.5,0.25);
 
         //dummy blocks below
@@ -56,7 +56,7 @@ export default class levelFour extends Phaser.Scene {
           this.target10=this.tube10.angle=180;
           this.tube10.setOrigin(0.75,0.25);
         this.tube11 = this.tubeGroup.create(322,483, "tubeA").setScale(.5).setInteractive();
-          this.target11=this.tube11.angle=90;
+          this.target11=this.tube11.angle=0;
         
         this.tube1.on('pointerdown', function (pointer) {
             if(this.target1+90>360){
